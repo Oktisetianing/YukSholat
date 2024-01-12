2d91c5238c0f2a6902f779c09f02b7d907c6f459
Oktisetianing <oktisetianing94@gmail.com>
2024-01-12 14:54:57 +0700

    tugas uas

---
+++ app/src/main/java/com/example/yuksholat/RegisterActivity.kt
@@ -0,0 +1,29 @@
+package com.example.yuksholat
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+
+class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var emailEditText: EditText
+    private lateinit var nameEditText: EditText
+    private lateinit var passwordEditText: EditText
+    private lateinit var rePasswordEditText: EditText
+    private lateinit var registerButton: Button
+
+    fun onSuksesRegister(view: View) {
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+        // Selesaikan aktivitas saat ini agar tidak bisa kembali lagi
+        finish()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.register)
+    }
+}
